30f8749eae6b15cfcdf87e14763a7e25eb3e6702
Mohamed Zamdin <mohamedzamdin@gmail.com>
2017-05-20 21:48:21 -0400

    Changed Remaining to Needed under the goal amount

--- madrasa/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.homepage, name='homepage'),
-    url(r'students$', views.students, name='students'),
-    url(r'classes$', views.madrasa_classes, name='classes'),
-]
--- registration/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.homepage, name='homepage'),
-    url(r'^date$', views.current_datetime, name='current_datetime'),
-]
