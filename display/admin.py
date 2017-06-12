4d4a6e1e8ec719a7e64da17e8024f3a9465fdfd8
Mohamed Zamdin <mohamedzamdin@gmail.com>
2017-06-11 23:20:34 -0400

    configuring the widgets on the bottom

--- display/admin.py
+++ display/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import FundGoal, MessageBar, CarouselEvents
+from .models import FundGoal, MessageBar, CarouselEvents, BoxWidgets
 
 # Register your models here.
 admin.site.register(FundGoal)
 admin.site.register(MessageBar)
 admin.site.register(CarouselEvents)
+admin.site.register(BoxWidgets)
